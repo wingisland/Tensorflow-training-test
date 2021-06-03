1edadcfecad70af41dab15613a38eee2e4a51857
haixei <aleksandraosuch002@gmail.com>
2021-06-03 12:11:13 +0200

    Add tuning with Keras Tuner

---
+++ keras-tuner.py
@@ -0,0 +1,56 @@
+from tensorflow.keras import layers as layers
+from tensorflow.keras import Sequential
+import tensorflow.keras.optimizers as Optimizer
+import kerastuner as kt
+from kerastuner import HyperModel, RandomSearch
+from main import train_ds, train_classes, getImages
+
+class RegressionHyperModel(HyperModel):
+    def build(self, hp):
+        model = Sequential()
+        model.add(layers.Conv2D(200, kernel_size=(3, 3), activation='relu'))
+        model.add(layers.Conv2D(180, kernel_size=(3, 3), activation='relu'))
+        model.add(layers.MaxPool2D(5, 5))
+        model.add(layers.Conv2D(180, kernel_size=(3, 3), activation='relu'))
+        model.add(layers.Conv2D(140, kernel_size=(3, 3), activation='relu'))
+        model.add(layers.Conv2D(100, kernel_size=(3, 3), activation='relu'))
+        model.add(layers.Conv2D(50, kernel_size=(3, 3), activation='relu'))
+        model.add(layers.MaxPool2D(5, 5))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(units=hp.Int('units',
+                                            min_value=100,
+                                            max_value=180,
+                                            step=40), activation='relu'))
+        model.add(layers.Dense(units=hp.Int('units',
+                                            min_value=80,
+                                            max_value=100,
+                                            step=10), activation='relu'))
+        model.add(layers.Dense(50, activation='relu'))
+        model.add(layers.Dropout(rate=0.5))
+        model.add(layers.Dense(6, activation='softmax'))
+
+
+        model.compile(optimizer=Optimizer.Adam(learning_rate=hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])), loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        return model
+
+# Initialize the model
+hypermodel = RegressionHyperModel()
+
+# Run the random search
+tuner_rs = RandomSearch(
+            hypermodel,
+            objective='val_accuracy',
+            max_trials=5,
+            executions_per_trial=1)
+
+tuner_rs.search_space_summary()
+tuner_rs.search(train_ds, train_classes, epochs=15, validation_split=0.30)
+
+# Get the best model
+best_model = tuner_rs.get_best_models(num_models=1)[0]
+
+# Evaluate
+val_ds, val_classes = getImages('../input/intel-image-classification/seg_test/seg_test/', 150)
+best_model.evaluate(val_ds, val_classes, verbose=1)
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from tensorflow.keras import layers
+from tensorflow.keras import layers as layers
 import tensorflow as tf
 from tensorflow.keras import Sequential
 from sklearn.utils import shuffle
-import tensorflow.keras.optimizers as optimizer
+import tensorflow.keras.optimizers as Optimizer
 import matplotlib.pyplot as plot
 import numpy as np
 import os
 
 # Check the GPU
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
 
 # Load the images and their labels into a data set
 def getImages(dataset_dir, img_size):
@@ -49,18 +50,18 @@ def getImages(dataset_dir, img_size):
     dataset_labels = np.array(dataset_labels)
     return dataset_array, dataset_labels, class_names
 
-# Get the training & validation data sets
+
+# Get the training data set
 train_ds, train_classes, class_names = getImages('./data/seg_train/seg_train/', 150)
 
 print("Training Data Array Shape :", train_ds.shape)
-print('Classes: ', class_names)
+print('Classes Shape: ', train_classes.shape)
 
 # Visualise the data we're working with
 plot.figure(figsize=(10, 10))
 for i in range(9):
     img = train_ds[i]
     img_label = class_names[train_classes[i]]
-    print(img)
 
     # Create a subplot for the image on the canvas
     ax = plot.subplot(3, 3, i + 1)
@@ -71,6 +72,7 @@ for i in range(9):
 # >> plot.show()
 
 # Creating the model
+# Creating the model
 model = Sequential()
 model.add(layers.Conv2D(200, kernel_size=(3, 3), activation='relu'))
 model.add(layers.Conv2D(180, kernel_size=(3, 3), activation='relu'))
@@ -78,43 +80,45 @@ model.add(layers.MaxPool2D(5, 5))
 model.add(layers.Conv2D(180, kernel_size=(3, 3), activation='relu'))
 model.add(layers.Conv2D(140, kernel_size=(3, 3), activation='relu'))
 model.add(layers.Conv2D(100, kernel_size=(3, 3), activation='relu'))
-model.add(layers.Conv2D(60, kernel_size=(3, 3), activation='relu'))
+model.add(layers.Conv2D(50, kernel_size=(3, 3), activation='relu'))
 model.add(layers.MaxPool2D(5, 5))
 model.add(layers.Flatten())
 model.add(layers.Dense(180, activation='relu'))
 model.add(layers.Dense(100, activation='relu'))
-model.add(layers.Dense(60, activation='relu'))
+model.add(layers.Dense(50, activation='relu'))
 model.add(layers.Dropout(rate=0.5))
 model.add(layers.Dense(6, activation='softmax'))
 
-model.compile(optimizer=optimizer.Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy',
+# Compile the model
+model.compile(optimizer=Optimizer.Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-epochs = 35
-history = model.fit(train_ds, train_classes, epochs=epochs, validation_split=0.30)
-print(model.summary())
+# Fit the model
+trained = model.fit(train_ds, train_classes, epochs=35, validation_split=0.30)
+model.summary()
 
 # Visualise the model accuracy
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+# Visualise the model accuracy
+acc = trained.history['accuracy']
+val_acc = trained.history['val_accuracy']
+loss = trained.history['loss']
+val_loss = trained.history['val_loss']
 
-epochs_range = range(epochs)
+epochs_range = range(35)
 
 # Create the graphs
 plot.figure(figsize=(10, 10))
 plot.subplot(1, 2, 1)
-plot.legend(loc='lower right')
 plot.title('Training & Validation Acc.')
 plot.plot(epochs_range, acc, label='Training Acc.')
 plot.plot(epochs_range, val_acc, label='Validation Acc.')
+plot.legend(['Train', 'Test'], loc='lower right')
 
 plot.subplot(1, 2, 2)
-plot.legend(loc='upper right')
 plot.title('Training & Validation Loss')
 plot.plot(epochs_range, loss, label='Training Loss')
 plot.plot(epochs_range, val_loss, label='Validation Loss')
+plot.legend(['Train', 'Test'], loc='upper right')
 
 # ..and show the graphs
-plot.show()
+# >> plot.show()
